45d9c304f6b9cc5aa437df0ba334407ee65ee6b0
Stuart Black <stuartblack@gmail.com>
2018-07-13 10:43:41 +0100

    Set db tables up in .sql file.

---
+++ db/cinema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS customers;
+
+
+CREATE TABLE customer (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+)
