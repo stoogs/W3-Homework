a78fa399741495eda2ee20daec8302ac413970aa
Stuart Black <stuartblack@gmail.com>
2018-07-13 11:26:25 +0100

    customer save to database tested ok

---
+++ console.rb
@@ -0,0 +1,11 @@
+require_relative('models/customers')
+require_relative('models/films')
+require_relative('models/tickets')
+
+film1 = Film.new("title" => "Dumb & Dumber", "price" => 10)
+p film1
+
+customer1 = Customer.new("name" => "Jermaine", "funds" => 100)
+p customer1
+customer1.save()
+# ticket1 = Ticket.new
--- models/films.rb
+++ models/films.rb
@@ -1,3 +1,37 @@
+require_relative('../db/sql_runner')
+
 class Film
+  attr_reader :id
+  attr_accessor :title, :price
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @price = options['price'].to_i
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end # class end
